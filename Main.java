ab5c38340cc5c8687a799580cf1610f1cc47466e
CHRuper <76441290+CHRuper@users.noreply.github.com>
2020-12-21 18:15:04 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,5 +5,7 @@ public class Main {
 
         System.out.println("Hello from Karol");
         System.out.println("Hello from Nikita <3");
+        System.out.println("Wasap from Roman");
+         
     }
 }
