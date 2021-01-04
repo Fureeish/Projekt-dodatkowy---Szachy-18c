9c14640e60b3b65dd018dd051f6deb2c80a477c2
Fureeish <f74.kwiatek@gmail.com>
2021-01-04 18:54:39 +0100

    Added more classes for Chess representation and provided an example of constructors and toStrings

---
+++ Board.java
@@ -0,0 +1,11 @@
+public class Board {
+    Tile[][] representation = new Tile[8][8];
+
+    public Board() {
+
+    }
+
+    public String toString() {
+        return "tmp";
+    }
+}
--- Main.java
+++ Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        Piece[] whitePawns = new Piece[8];
-        for (int i = 0; i < whitePawns.length; i++) {
-            Piece newPiece = new Piece();
-            newPiece.type = Type.PAWN;
-            newPiece.color = Color.WHITE;
-            whitePawns[i] = newPiece;
-        }
+        Board board = new Board();
+        Board board1 = new Board();
+        Board board2 = new Board();
+        Board board3 = new Board();
+
+        System.out.println(board);
+
+//        Piece blackRook = new Piece();
+//        blackRook.color = Color.BLACK;
+//        blackRook.type = Type.ROOK;
+//
+//        board.representation[0][0] = new Tile();
+//        board.representation[0][0].piece = blackRook;
     }
 }
