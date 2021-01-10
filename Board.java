f0ebd45f53efd3b8c35965c147ecafd3c07b2751
maciejzelias <76003342+maciejzelias@users.noreply.github.com>
2021-01-11 00:31:42 +0100

    Second Update of ToStringBoard
    I updated toStringBoard with StringBuilder. Unfortunetally, it isn't still working well. I have errors : cannot access piece/type in Board.java . And after starting the program i see that : 
            Exception in thread "main" java.lang.NullPointerException
            at Board.toString(Board.java:15)
    	at java.base/java.lang.String.valueOf(String.java:3388)
    	at java.base/java.io.PrintStream.println(PrintStream.java:1047)
    	at Main.main(Main.java:8)

--- Board.java
+++ Board.java
@@ -1,5 +1,3 @@
-import java.awt.Color;
-
 public class Board {
     Tile[][] representation = new Tile[8][8];
 
@@ -7,50 +5,50 @@ public class Board {
 
     }
 
- /*   for(int i=0;i<8;i++)
-    {
-        for(int b=0;b<8;b++)
-        {
-            if(representation[i][b].piece.color==Color.BLACK)
-            {
-                if(representation[i][b].piece.type==Type.KING)
-                    System.out.print("\u001b[38;2;0;0;0m♚");
-                if(representation[i][b].piece.type==Type.BISHOP)
-                    System.out.print("\u001b[38;2;0;0;0m♝");
-                if(representation[i][b].piece.type==Type.ROOK)
-                    System.out.print("\u001b[38;2;0;0;0m♜");
-                if(representation[i][b].piece.type==Type.QUEEN)
-                    System.out.print("\u001b[38;2;0;0;0m♛");
-                if(representation[i][b].piece.type==Type.KNIGHT)
-                    System.out.print("\u001b[38;2;0;0;0m♞");
-                if(representation[i][b].piece.type==Type.PAWN)
-                    System.out.print("\u001b[38;2;0;0;0m♟");
-            }
-            if(representation[i][b].piece.color == Color.WHITE)
-            {
-                if(representation[i][b].piece.type==Type.KING)
-                    System.out.print("\u001b[38;2;255;255;255m♚");
-                if(representation[i][b].piece.type==Type.BISHOP)
-                    System.out.print("\u001b[38;2;255;255;255m♝");
-                if(representation[i][b].piece.type==Type.ROOK)
-                    System.out.print("\u001b[38;2;255;255;255m♜");
-                if(representation[i][b].piece.type==Type.QUEEN)
-                    System.out.print("\u001b[38;2;255;255;255m♛");
-                if(representation[i][b].piece.type==Type.KNIGHT)
-                    System.out.print("\u001b[38;2;255;255;255m♞");
-                if(representation[i][b].piece.type==Type.PAWN)
-                    System.out.print("\u001b[38;2;255;255;255m♟");
-
-            }
-            else
-                System.out.print("puste pole");
-        }
-        System.out.println();
-    }
-
-  */
     public String toString() {
-        return ("\u001b[48;2;255;255;255m\u001b[38;2;0;0;0m♚");
+        String board="";
+        StringBuilder sB = new StringBuilder(board);
+        for(int i=0;i<8;i++)
+        {
+            for(int b=0;b<8;b++)
+            {
+                if(representation[i][b].piece.color == Color.BLACK)
+                {
+                    if(representation[i][b].piece.type==Type.KING)
+                        sB.append("\u001b[38;2;0;0;0m♚");
+                    if(representation[i][b].piece.type==Type.BISHOP)
+                        sB.append("\u001b[38;2;0;0;0m♝");
+                    if(representation[i][b].piece.type==Type.ROOK)
+                        sB.append("\u001b[38;2;0;0;0m♜");
+                    if(representation[i][b].piece.type==Type.QUEEN)
+                        sB.append("\u001b[38;2;0;0;0m♛");
+                    if(representation[i][b].piece.type==Type.KNIGHT)
+                        sB.append("\u001b[38;2;0;0;0m♞");
+                    if(representation[i][b].piece.type==Type.PAWN)
+                        sB.append("\u001b[38;2;0;0;0m♟");
+                }
+                if(representation[i][b].piece.color == Color.WHITE)
+                {
+                    if(representation[i][b].piece.type==Type.KING)
+                        sB.append("\u001b[38;2;255;255;255m♚");
+                    if(representation[i][b].piece.type==Type.BISHOP)
+                        sB.append("\u001b[38;2;255;255;255m♝");
+                    if(representation[i][b].piece.type==Type.ROOK)
+                        sB.append("\u001b[38;2;255;255;255m♜");
+                    if(representation[i][b].piece.type==Type.QUEEN)
+                        sB.append("\u001b[38;2;255;255;255m♛");
+                    if(representation[i][b].piece.type==Type.KNIGHT)
+                        sB.append("\u001b[38;2;255;255;255m♞");
+                    if(representation[i][b].piece.type==Type.PAWN)
+                        sB.append("\u001b[38;2;255;255;255m♟");
+
+                }
+                else
+                    sB.append("□");
+            }
+            sB.append("\n");
+        }
+        board=sB.toString();
+        return board;
     }
 }
-
