d7e8b45f35598da276a6da254fc7d3bdeb435653
maciejzelias <76003342+maciejzelias@users.noreply.github.com>
2021-01-11 18:59:07 +0100

    Update ToString Board
    Added check if representation is null firstly

--- Board.java
+++ Board.java
@@ -1,9 +1,11 @@
 public class Board {
+    
     Tile[][] representation = new Tile[8][8];
 
     public Board() {
 
     }
+
 
     public String toString() {
         String board="";
@@ -12,39 +14,41 @@ public class Board {
         {
             for(int b=0;b<8;b++)
             {
-                if(representation[i][b].piece.color == Color.BLACK)
+                if(representation[i][b]==null)
                 {
-                    if(representation[i][b].piece.type==Type.KING)
-                        sB.append("\u001b[38;2;0;0;0m♚");
-                    if(representation[i][b].piece.type==Type.BISHOP)
-                        sB.append("\u001b[38;2;0;0;0m♝");
-                    if(representation[i][b].piece.type==Type.ROOK)
-                        sB.append("\u001b[38;2;0;0;0m♜");
-                    if(representation[i][b].piece.type==Type.QUEEN)
-                        sB.append("\u001b[38;2;0;0;0m♛");
-                    if(representation[i][b].piece.type==Type.KNIGHT)
-                        sB.append("\u001b[38;2;0;0;0m♞");
-                    if(representation[i][b].piece.type==Type.PAWN)
-                        sB.append("\u001b[38;2;0;0;0m♟");
-                }
-                if(representation[i][b].piece.color == Color.WHITE)
-                {
-                    if(representation[i][b].piece.type==Type.KING)
-                        sB.append("\u001b[38;2;255;255;255m♚");
-                    if(representation[i][b].piece.type==Type.BISHOP)
-                        sB.append("\u001b[38;2;255;255;255m♝");
-                    if(representation[i][b].piece.type==Type.ROOK)
-                        sB.append("\u001b[38;2;255;255;255m♜");
-                    if(representation[i][b].piece.type==Type.QUEEN)
-                        sB.append("\u001b[38;2;255;255;255m♛");
-                    if(representation[i][b].piece.type==Type.KNIGHT)
-                        sB.append("\u001b[38;2;255;255;255m♞");
-                    if(representation[i][b].piece.type==Type.PAWN)
-                        sB.append("\u001b[38;2;255;255;255m♟");
-
-                }
-                else
                     sB.append("□");
+                }
+                else {
+                    if (representation[i][b].piece.color == Color.BLACK) {
+                        if (representation[i][b].piece.type == Type.KING)
+                            sB.append("\u001b[38;2;0;0;0m♚");
+                        if (representation[i][b].piece.type == Type.BISHOP)
+                            sB.append("\u001b[38;2;0;0;0m♝");
+                        if (representation[i][b].piece.type == Type.ROOK)
+                            sB.append("\u001b[38;2;0;0;0m♜");
+                        if (representation[i][b].piece.type == Type.QUEEN)
+                            sB.append("\u001b[38;2;0;0;0m♛");
+                        if (representation[i][b].piece.type == Type.KNIGHT)
+                            sB.append("\u001b[38;2;0;0;0m♞");
+                        if (representation[i][b].piece.type == Type.PAWN)
+                            sB.append("\u001b[38;2;0;0;0m♟");
+                    }
+                    if (representation[i][b].piece.color == Color.WHITE) {
+                        if (representation[i][b].piece.type == Type.KING)
+                            sB.append("\u001b[38;2;255;255;255m♚");
+                        if (representation[i][b].piece.type == Type.BISHOP)
+                            sB.append("\u001b[38;2;255;255;255m♝");
+                        if (representation[i][b].piece.type == Type.ROOK)
+                            sB.append("\u001b[38;2;255;255;255m♜");
+                        if (representation[i][b].piece.type == Type.QUEEN)
+                            sB.append("\u001b[38;2;255;255;255m♛");
+                        if (representation[i][b].piece.type == Type.KNIGHT)
+                            sB.append("\u001b[38;2;255;255;255m♞");
+                        if (representation[i][b].piece.type == Type.PAWN)
+                            sB.append("\u001b[38;2;255;255;255m♟");
+
+                    }
+                }
             }
             sB.append("\n");
         }
